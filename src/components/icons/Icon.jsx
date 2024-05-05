f3c901e92f9692bcc3d897fc59779e24422149ee
Amirhossein <4mirh03einhashemi@gmail.com>
2024-05-05 23:44:01 +0330

    add some icons

--- src/components/icons/Icon.jsx
+++ src/components/icons/Icon.jsx
@@ -1,26 +1,49 @@
 import styled, {css} from "styled-components";
 
 const Icon = styled.svg`
-  width: 2.4rem;
-  height: 2.4rem;
-
   ${(props) =>
     props.type === "primary" &&
     css`
+      width: 2.4rem;
+      height: 2.4rem;
+      stroke: var(--color-gray-800);
+    `}
+
+  ${(props) =>
+    props.type === "primarySmall" &&
+    css`
+      width: 1.8rem;
+      height: 1.8rem;
       stroke: var(--color-gray-800);
     `}
 
   ${(props) =>
     props.type === "secondary" &&
     css`
+      width: 2.4rem;
+      height: 2.4rem;
       stroke: var(--color-gray-400);
     `}
 
   ${(props) =>
+    props.type === "secondarySmall" &&
+    css`
+      width: 1.8rem;
+      height: 1.8rem;
+      stroke: var(--color-gray-400);
+    `}
+
+    ${(props) =>
     props.type === "send" &&
     css`
+      width: 2.4rem;
+      height: 2.4rem;
       fill: var(--color-blue-400);
     `}
 `;
+
+Icon.defaultProps = {
+  type: "primary",
+};
 
 export default Icon;
