c2839949d4edee99501e036d435d6ed79b533c7c
Amirhossein <4mirh03einhashemi@gmail.com>
2024-05-08 22:27:47 +0330

    add icons

---
+++ src/components/icons/ChevRight.jsx
@@ -0,0 +1,21 @@
+import Icon from "./Icon";
+
+function ChevRight() {
+  return (
+    <Icon
+      type="primarySmall"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m8.25 4.5 7.5 7.5-7.5 7.5"
+      />
+    </Icon>
+  );
+}
+
+export default ChevRight;
